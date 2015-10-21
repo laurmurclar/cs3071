feb68962225a1a03b890cb091439263a08c8147b
Laura Murphy-Clarkin <laurmurclar@gmail.com>
2015-10-21 13:24:16 +0100

    Make code runnable
    Corrected syntax errors. Corrected state transitions.

--- lab-1.c
+++ lab-1.c
@@ -1,12 +1,17 @@
 #include <string.h>
 #include <stdlib.h>
-// TODO: Function which works out whether to call get next state or the end function
+#include <stdio.h>
+#include <ctype.h>
 
+int currentState = 1;
 /* Transitions to given state. Updates the current state. Advances the input
 */
 int go_to_state(int state)
 {
 
+	currentState = state;
+	// Perhaps doing some other stuff
+	return 0;
 }
 
 /* function which given the input and the current state, 
@@ -35,18 +40,16 @@ int get_next_state(int current_state, int inputType)
 				go_to_state(4);
 			} else if (inputType == 2) { // b,B
 				go_to_state(3);
-			} else if (inputType == 3) { // aA,cC,dD,eE
-				go_to_state(5);
-			} else if (inputType == 4) { // h,H
+			} else if (inputType == 3) { // h,H
 				go_to_state(6);
-			} else {
-				//ERROR
+			} else if (inputType == 4) { //  aA,cC,dD,eE
+				go_to_state(5);
 			}
 			break;
 		case 3:
-			if (inputType == 0 || inputType == 1 || inputType == 2 || inputType == 3) { // 0-7, 8 or 9, b,B, aA,cC,dD,eE
+			if (inputType == 0 || inputType == 1 || inputType == 2 || inputType == 4) { // 0-7, 8 or 9, b,B, aA,cC,dD,eE
 				go_to_state(5);
-			} else if (inputType == 4) { // h,H
+			} else if (inputType == 3) { // h,H
 				go_to_state(6);
 			} else {
 				//ERROR
@@ -55,18 +58,18 @@ int get_next_state(int current_state, int inputType)
 		case 4:
 			if (inputType == 0 || inputType == 1) { // 0-7, 8 or 9
 				go_to_state(4);
-			} else if (inputType == 2 || inputType == 3) { // b,B, aA,cC,dD,eE
+			} else if (inputType == 2 || inputType == 4) { // b,B, aA,cC,dD,eE
 				go_to_state(5);
-			} else if (inputType == 4) { // h,H
+			} else if (inputType == 3) { // h,H
 				go_to_state(6);
 			} else {
 				//ERROR
 			}
 			break;
 		case 5:
-			if (inputType == 0 || inputType == 1 || inputType == 2 || inputType == 3) { // 0-7, 8 or 9, b,B, aA,cC,dD,eE
+			if (inputType == 0 || inputType == 1 || inputType == 2 || inputType == 4) { // 0-7, 8 or 9, b,B, aA,cC,dD,eE
 				go_to_state(5);
-			} else if (inputType == 4) { // h,H
+			} else if (inputType == 3) { // h,H
 				go_to_state(6);
 			} else {
 				//ERROR
@@ -87,6 +90,7 @@ int get_next_state(int current_state, int inputType)
 			//Error
 			break;
 	}
+	return 0;
 }
 
 /* inputTypes 
@@ -110,9 +114,9 @@ int get_input_type(char c){
 	} else if (isalpha(c)) {
 		if (c == 'b' || c == 'B') return 2;
 		else if (c == 'h' || c == 'H') return 3;
-		else if (c >= 'a' && c <= 'e' || c >= 'A' && c<= 'E') return 4;
+		else if ((c >= 'a' && c <= 'e') || (c >= 'A' && c<= 'E')) return 4;
 	} else if (c == '-' || c == '+') return 5;
-	else return 6;
+	return 6;
 }
 
 // Checks if the current state is a valid end state for an integer input
@@ -133,40 +137,40 @@ int is_oct_end_state()
 	return (currentState == 3);
 }
 
-void print_decimal_of_int(string)
+void print_decimal_of_int(char sval[])
 {
-
+	printf("Int: %s\n", sval);
 }
 
-void print_decimal_of_hex(string)
+void print_decimal_of_hex(char sval[])
 {
-
+	printf("Hex: %s\n", sval);
 }
 
-void print_decimal_of_oct(string)
+void print_decimal_of_oct(char sval[])
 {
-
+	printf("Oct: %s\n", sval);
 }
-int main(){
-	int currentState = 1;
+
+int main() {
+	char input[20];
+
 	// get input
-	char input[];
-
 	printf("Enter a value: ");
 	gets(input);
-	int lengthOfInput = input.strlen();
+	int lengthOfInput = strlen(input);
 
 	for (int i = 0; i < lengthOfInput; i++) { // for each char in the input
-		// work out in type
+		// work out input type
 		int inType = get_input_type(input[i]);
 		// get next state
 		get_next_state(currentState, inType);
 	} // hit end marker
-	if (!is_int_end_state() && !is_hex_end_state() && !is_oct_end_state()) { // any of the end states
+	if (!is_int_end_state() && !is_hex_end_state() && !is_oct_end_state()) { // not any of the valid end states
 		// error, not a valid input
 		printf("Error: Not a valid input.\n");
 		EXIT_FAILURE;
-	} else {
+	} else { // it is a valid end state
 		// calculate the decimal value & print it
 		if (is_int_end_state()) print_decimal_of_int(input);
 		else if (is_hex_end_state()) print_decimal_of_hex(input);
